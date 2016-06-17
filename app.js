9138d926b9492b8cf7509e38196d58ad1abb0611
Adam Zerner <azerner3@gmail.com>
2016-06-17 12:08:52 -0700

    MVP seems good

--- app.js
+++ app.js
@@ -6,18 +6,19 @@
     .controller('MainCtrl', MainCtrl)
   ;
 
-  function MainCtrl(Players, DataService) {
+  function MainCtrl(Players, DataService, WinSharesPerSeasonPerDraftPick) {
     var vm = this;
     vm.draft = '2003';
     vm.drafts = ['2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'];
     vm.userChoices = constructUserChoices();
-    vm.pointsPerDraft = constructPointsPerDraft();
-    vm.players = Players;
+    vm.userPointsForEachDraft = constructUserPointsForEachDraft();
+    vm.PLAYERS = Players;
+    vm.WIN_SHARES_PER_SEASON_PER_DRAFT_PICK = WinSharesPerSeasonPerDraftPick;
 
     function constructUserChoices() {
       /*
         {
-          2003: [{ name, points }, {}, ... {}],
+          2003: [{ name, points, winSharesPerSeason }, {}, ... {}],
           ...
           2015: [{}, {}, ... {}],
         }
@@ -31,6 +32,7 @@
           userChoices[draft].push({
             name: null,
             points: null,
+            winSharesPerSeason: null,
           });
         }
       });
@@ -38,7 +40,7 @@
       return userChoices;
     }
 
-    function constructPointsPerDraft() {
+    function constructUserPointsForEachDraft() {
       /*
         [
           2003: null,
@@ -47,35 +49,69 @@
         ]
       */
 
-      var pointsPerDraft = {};
+      var userPointsForEachDraft = {};
 
       vm.drafts.forEach(function (draft) {
-        pointsPerDraft[draft] = null;
+        userPointsForEachDraft[draft] = null;
       });
 
-      return pointsPerDraft;
+      return userPointsForEachDraft;
     }
 
-    vm.setPointsForPick = function (name, yourSpot) {
+    vm.getPickMultiplyer = function (pick) {
+      return DataService._getBenPickMultiplier(pick);
+    };
+
+    vm.onSelect = function (name, pick) {
+      vm.setPointsForPick(name, pick);
+      vm.setWSPSForPick(name, pick);
+      vm.setPointsForDraft();
+    };
+
+    vm.getPointsForPick = function (name, yourSpot) {
       if (!name) {
         vm.userChoices[vm.draft][yourSpot - 1].points = null;
         return;
       }
 
-      var playerAWSPS;
+      var playerWSPS;
       var actualSpot;
 
       Players[vm.draft].forEach(function (playerObj, index) {
         if (playerObj.name === name) {
-          playerAWSPS = playerObj.winSharesPerSeason;
+          playerWSPS = playerObj.winSharesPerSeason;
           actualSpot = index + 1;
           return;
         }
       });
 
       if (vm.userChoices[vm.draft] && vm.userChoices[vm.draft][yourSpot - 1]) {
-        var diff = DataService.getWeightedAWSPSDiff(playerAWSPS, yourSpot, actualSpot);
-        vm.userChoices[vm.draft][yourSpot - 1].points = diff;
+        return DataService.getWeightedWSPSDiff(playerWSPS, yourSpot, actualSpot);
+      }
+    };
+
+    vm.setPointsForPick = function (name, yourSpot) {
+      var diff = vm.getPointsForPick(name, yourSpot);
+      vm.userChoices[vm.draft][yourSpot - 1].points = diff;
+    };
+
+    vm.setWSPSForPick = function (name, pick) {
+      if (!name) {
+        vm.userChoices[vm.draft][pick - 1].winSharesPerSeason = null;
+        return;
+      }
+
+      var playerWSPS;
+
+      Players[vm.draft].forEach(function (playerObj, index) {
+        if (playerObj.name === name) {
+          playerWSPS = playerObj.winSharesPerSeason;
+          return;
+        }
+      });
+
+      if (vm.userChoices[vm.draft] && vm.userChoices[vm.draft][pick - 1]) {
+        vm.userChoices[vm.draft][pick - 1].winSharesPerSeason = playerWSPS;
       }
     };
 
@@ -90,30 +126,34 @@
         }
       }
 
-      vm.pointsPerDraft[vm.draft] = totalPoints / choicesMade;
+      if (choicesMade) {
+        vm.userPointsForEachDraft[vm.draft] = totalPoints / choicesMade;
+      } else {
+        vm.userPointsForEachDraft[vm.draft] = '';
+      }
     };
 
     vm.getPartialDraftActualPoints = function () {
-      debugger;
-
       var totalPoints = 0;
       var choicesMade = 0;
 
       for (var i = 0, len = vm.userChoices[vm.draft].length; i < len; i++) {
         if (vm.userChoices[vm.draft][i].points) {
-          totalPoints += DataService.getWeightedAWSPSDiff(Players[vm.draft][i].winSharesPerSeason, i + 1, i + 1);
+          totalPoints += DataService.getWeightedWSPSDiff(Players[vm.draft][i].winSharesPerSeason, i + 1, i + 1);
           choicesMade++;
         }
       }
 
-      return totalPoints / choicesMade;
+      if (choicesMade) {
+        return totalPoints / choicesMade;
+      }
     };
 
     vm.getFullDraftActualPoints = function () {
       var totalPoints = 0;
 
       Players[vm.draft].forEach(function (player, i) {
-        totalPoints += DataService.getWeightedAWSPSDiff(player.winSharesPerSeason, i + 1, i + 1);
+        totalPoints += DataService.getWeightedWSPSDiff(player.winSharesPerSeason, i + 1, i + 1);
       });
 
       return totalPoints / 60;
